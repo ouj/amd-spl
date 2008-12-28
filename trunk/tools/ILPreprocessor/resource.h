dc3b367b9c6e90fc69411c3b44e4b78d10f6668e
jiawei.ou <jiawei.ou@1960d7c4-c739-11dd-8829-37334faa441c>
2008-12-28 13:47:10 +0000

    Add a IL source pre-process tool to accelerate my development. I hope SA and KA will remove that stupid bug in next release...

---
+++ trunk/tools/ILPreprocessor/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by ILPreprocessor.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_ILPREPROCESSOR_DIALOG       102
+#define IDS_OPENFILE_FILTERSTRING       102
+#define IDS_LOAD_ILSRC_ERROR            103
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT1                       1000
+#define IDC_CPP_ILSRC_EDIT              1000
+#define IDC_FILEPATH_EDIT               1001
+#define IDC_BROWSE                      1002
+#define IDC_RELOAD                      1003
+#define IDC_COPY_TO_CLIPBROAD           1004
+#define IDC_ABOUT                       1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
