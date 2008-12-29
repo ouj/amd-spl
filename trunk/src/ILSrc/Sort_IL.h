05ab39b77913d221d1c2572f070567c38a67de5f
jiawei.ou <jiawei.ou@1960d7c4-c739-11dd-8829-37334faa441c>
2008-12-29 06:25:07 +0000

    BitonicSort Completed! But address translation is not supported.

--- trunk/src/ILSrc/Sort_IL.h
+++ trunk/src/ILSrc/Sort_IL.h
@@ -8,7 +8,6 @@ const char f_szBitonicSort[] =
 "dcl_output_generic o0\n"
 "dcl_resource_id(0)_type(2d,unnorm)_fmtx(float)_fmty(float)_fmtz(float)_fmtw(float)\n"
 "dcl_input_position_interp(linear_noperspective) v0.xy__\n"
-"; l0 = (0.500000f 2.000000f 0.000000f 1.000000f)\n"
 "dcl_literal l0, 0x3F000000, 0x40000000, 0x00000000, 0x3F800000\n"
 "dcl_cb cb0[1]\n"
 "mov r1.xy__, v0\n"
@@ -16,18 +15,24 @@ const char f_szBitonicSort[] =
 "ftoi r1, r1\n"
 "itof r1.x___, r1.x000\n"
 "mod r3.x___, r1.x000, cb0[0].z000\n"
-"sub r3.x___, r3.x000, cb0[0].y000\n"
-"sub r3.x___, r3.x000, l0.x000\n"
-"sgn r3.x___, r3.x000\n"
-"div_zeroop(infinity) r4.x___, r1.x000, cb0[0].x000\n"
+"lt r3.x___, r3.x000, cb0[0].y000\n"
+"cmov_logical r3.x___, r3.x000, l0.w000, l0.w000_neg(xyzw)\n"
+"div_zeroop(infinity) r4.x___, r1.x000, cb[0].x000\n"
 "round_neginf r4.x___, r4.x000\n"
 "mod r4.x___, r4.x000, l0.y000\n"
 "eq r4.x___, r4.x000, l0.z000\n"
 "cmov_logical r4.x___, r4.x000, l0.w000, l0.w000_neg(xyzw)\n"
-"mov o0.x___, r4.x000\n"
+"mul_ieee r2.x___, r3.x000, cb0[0].y000\n"
+"add r2.x___, r2.x000, r1.x000\n"
+"sample_resource(0)_sampler(0) r11, r1.xy00\n"
+"sample_resource(0)_sampler(0) r22, r2.xy00\n"
+"lt r33, r11, r22\n"
+"cmov_logical r5, r33, r11, r22\n"
+"cmov_logical r6, r33, r22, r11\n"
+"eq r44, r3.x000, r4.x000\n"
+"cmov_logical o0, r44, r5, r6\n"
 "endmain\n"
 "end\n";
-
 
 amdspl::ILProgInfo BitonicSortILInfo(void)
 {
