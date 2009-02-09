121ea12d64f3a0419f9d6bef65206ecb630917be
jiawei.ou <jiawei.ou@1960d7c4-c739-11dd-8829-37334faa441c>
2009-02-09 06:50:46 +0000

    Begin to renew the runtime

--- src/include/amdspl.h
+++ src/include/amdspl.h
@@ -1,6 +1,12 @@
-#ifndef _AMD_SPL_LIBRARY_
-#define _AMD_SPL_LIBRARY_
-
+#if !defined(_AMDSPL_H)
+#define _AMDSPL_H
+//
+//  @ Project : AMD-SPL
+//  @ File Name : AmdSpl.h
+//  @ Date : 2009/2/9
+//  @ Author : Jiawei Ou
+//
+//
 ////////////////////////////////////////////////////////////////////////////////
 //!
 //! \file amdspl.h
@@ -10,7 +16,8 @@
 //!
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "SplBitonicSort.h"
+
+#include "cal.h"
 
 #ifdef AMD_SPL_EXPORT
 #define SPL_EXPORT __declspec(dllexport)
@@ -22,24 +29,23 @@
 //!
 //! \brief The namespace of AMD-SPL
 //!
-//////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////
 namespace amdspl
 {
-    //////////////////////////////////////////////////////////////////////////
-    //! \brief This the initialization function of the amd-spl library.
-    //!
-    //! This function should be the first function call of the amd-spl
-    //! library. (Note: currently this function does nothing)
-    //////////////////////////////////////////////////////////////////////////
-	bool SPL_EXPORT initialSPL();
+	struct SPL_EXPORT DEVICE_LIST_ITEM
+	{
+	public:
+		DEVICE_LIST_ITEM(unsigned short deviceId = 0, CALdevice deviceHandle = 0);
+		unsigned short deviceId;
+		CALdevice deviceHandle;
+	};
 
-    //////////////////////////////////////////////////////////////////////////
-    //! \brief This is the clean up function of the amd-spl library.
-    //!
-    //! This function should be the last function call of the amd-spl
-    //! library. (Note: currently this function does nothing)
-    //////////////////////////////////////////////////////////////////////////
-    bool SPL_EXPORT cleanupSPL();
+    class SPL_EXPORT AmdSpl
+    {
+    public:
+        static bool InitializeSPL(DEVICE_LIST_ITEM *devices = 0, unsigned short numDevices = 0, int defaultDeviceIdx = 0);
+        static bool CleanupSPL();
+    };
 }
 
-#endif
+#endif  //_AMDSPL_H
