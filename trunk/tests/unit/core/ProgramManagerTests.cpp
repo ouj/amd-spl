3067ff583931e491754826334ab1f06d7cb38de2
jiawei.ou <jiawei.ou@1960d7c4-c739-11dd-8829-37334faa441c>
2009-02-26 05:58:55 +0000

    Runtime coding completed! Unit testing...

--- trunk/tests/unit/core/ProgramManagerTests.cpp
+++ trunk/tests/unit/core/ProgramManagerTests.cpp
@@ -7,15 +7,19 @@ using namespace amdspl::core::cal;
 
 typedef RuntimeTestFixture ProgramManagerTests;
 
-typedef ProgramInfo<1, 2, 3, false> TestProgamInfo;
-
-DCL_PROGRAM_ID(TestProgamInfo) = "Test Program Info";
-DCL_PROGRAM_SOURCE(TestProgamInfo) = 
+typedef ProgramInfo<1, 2, 3, false> TestProgramInfo;
+static const char *TestProgramSource = 
 "This is a test source file, I am not going to compile it.\n";
+static const TestProgramInfo testProgInfo = 
+    TestProgramInfo("Test Program Info",TestProgramSource);
+
+static const TestProgramInfo testRepProgInfo = 
+    TestProgramInfo("Test Replacement Program Info",TestProgramSource)
+    .replaceTkn("test", "replacement test")
+    .replaceTkn("compile", "run");
 
 typedef ProgramInfo<1, 1, 1, true> SampleProgram;
-DCL_PROGRAM_ID(SampleProgram) = "Sample Program Info";
-DCL_PROGRAM_SOURCE(SampleProgram) = 
+static const char* _sz_sample_prog_source_ = 
 "il_ps_2_0\n"
 "dcl_output_generic o0\n"
 "dcl_input_position_interp(linear_noperspective) v0.xy__\n"
@@ -26,16 +30,24 @@ DCL_PROGRAM_SOURCE(SampleProgram) =
 "add o0, r1, cb0[0]\n"
 "endmain\n"
 "end\n";
+static const SampleProgram sampleProgram = 
+    SampleProgram("Sample Program Info", _sz_sample_prog_source_);
 
 TEST_F(ProgramManagerTests, ProgramInfoTest)
 {
-    ASSERT_EQ(1, TestProgamInfo::outputs);
-    ASSERT_EQ(2, TestProgamInfo::inputs);
-    ASSERT_EQ(3, TestProgamInfo::constants);
-    ASSERT_EQ(false, TestProgamInfo::global);
+    ASSERT_EQ(1, testProgInfo.outputs);
+    ASSERT_EQ(2, testProgInfo.inputs);
+    ASSERT_EQ(3, testProgInfo.constants);
+    ASSERT_EQ(0, testProgInfo.global);
 
-    ASSERT_TRUE(!strcmp(TestProgamInfo::source, "This is a test source file, I am not going to compile it.\n"));
-    ASSERT_TRUE(!strcmp(TestProgamInfo::programID, "Test Program Info"));
+    ASSERT_STREQ("This is a test source file, I am not going to compile it.\n", testProgInfo.getSource());
+    ASSERT_STREQ("Test Program Info" ,testProgInfo.getID());
+}
+
+TEST_F(ProgramManagerTests, ProgramTokenReplacementTest)
+{
+    ASSERT_STREQ("This is a replacement test source file, I am not going to run it.\n", testRepProgInfo.getSource());
+    ASSERT_STREQ("Test Replacement Program Info", testRepProgInfo.getID());
 }
 
 
@@ -49,7 +61,7 @@ TEST_F(ProgramManagerTests, LoadUnloadProgramTest)
     Device* device = _deviceMgr->getDefaultDevice();
     ASSERT_TRUE(device);
     Program *prog = 
-        ProgramManagerTests::_progMgr->loadProgram<SampleProgram>(device);
+        ProgramManagerTests::_progMgr->loadProgram(sampleProgram, device);
     ASSERT_TRUE(prog != NULL);
 
     _progMgr->unloadProgram(prog);
